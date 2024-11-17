407cf8836a6e0fc739353d4379a8af4b5b66df7c
iiiamken <kenny.t.dinh93@gmail.com>
2024-11-17 19:32:48 +0100

    created navar

--- src/app/dashboard/DashboardWrapper.tsx
+++ src/app/dashboard/DashboardWrapper.tsx
@@ -1,11 +1,17 @@
+import Navbar from "../(components)/Navbar/Navbar"
+
 export default function DashboardWrapper({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="flex bg-gray-50 text-gray-900 w-full min-h-screen">
-      Dashboard {children}
+    <div className="light flex bg-gray-50 text-gray-900 w-full min-h-screen">
+      SIDEBAR
+      <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md: pl-24">
+        <Navbar />
+        {children}
+      </main>
     </div>
   )
 }
