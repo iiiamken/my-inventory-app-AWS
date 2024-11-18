daa1be4c97bcc693a25bc7a32b4737b77cb243f9
iiiamken <kenny.t.dinh93@gmail.com>
2024-11-18 23:14:05 +0100

    added Logo in sidebar

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,3 +1,21 @@
+"use client"
+
+import { Menu } from "lucide-react"
+
 export default function Sidebar() {
-  return <div>Sidebar</div>
+  return (
+    <div>
+      {/* Logo */}
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>LOGO</div>
+        <h1 className="font-extrabold text-2xl">KENSTOCK</h1>
+      </div>
+      <button
+        className="md:hidden  px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+        onClick={() => {}}
+      >
+        <Menu className="w-4 h-4" />
+      </button>
+    </div>
+  )
 }
