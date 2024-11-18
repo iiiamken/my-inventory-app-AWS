203898ff8a587c4b67c3bb73a43ea242bfdd02e6
iiiamken <kenny.t.dinh93@gmail.com>
2024-11-18 23:20:59 +0100

    skeleton code done in sidebar

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -9,13 +9,19 @@ export default function Sidebar() {
       <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
         <div>LOGO</div>
         <h1 className="font-extrabold text-2xl">KENSTOCK</h1>
+        <button
+          className="md:hidden  px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
       </div>
-      <button
-        className="md:hidden  px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-        onClick={() => {}}
-      >
-        <Menu className="w-4 h-4" />
-      </button>
+      {/*Links*/}
+      <div className="felx-grow mt-8">{/*Links here*/}</div>
+      {/*Footer*/}
+      <div>
+        <p className="text-center text-xs text-gray-500">© 2022 Kenstock</p>
+      </div>
     </div>
   )
 }
