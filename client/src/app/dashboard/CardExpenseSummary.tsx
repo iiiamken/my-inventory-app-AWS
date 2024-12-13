5430ea52a0d08aa1a40de4a66d285ef16b32401b
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-13 23:45:52 +0100

    added calculations and piechart

--- client/src/app/dashboard/CardExpenseSummary.tsx
+++ client/src/app/dashboard/CardExpenseSummary.tsx
@@ -1,9 +1,31 @@
 import { useGetDashboadDataQuery } from "@/state/api"
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts"
 const colors = ["#00C49F", "#0088FE", "#FFBB28"]
 
-export default function CardExpenseSummary() {
-  const { data, isLoading } = useGetDashboadDataQuery()
+type ExpenseSums = {
+  [category: string]: number
+}
 
+export default function CardExpenseSummary() {
+  const { data: dashboardData, isLoading } = useGetDashboadDataQuery()
+
+  const expenseByCategorySummary = dashboardData?.expenseByCategorySummary || []
+  const expenseSums = expenseByCategorySummary.reduce(
+    (acc: ExpenseSums, item: ExpenseByCategorySummary) => {
+      const category = item.category + " Expenses"
+      const amount = parseInt(item.amount, 10)
+      if (!acc.category) acc.category = 0
+      acc.category += amount
+      return acc
+    }
+  )
+
+  const expenseCategories = Object.entries(expenseSums).map(
+    ([name, value]) => ({
+      name,
+      value,
+    })
+  )
   return (
     <div className="row-span-3 bg-white shadow-md  rounded-2xl flex flex-col justify-between">
       {isLoading ? (
@@ -16,6 +38,34 @@ export default function CardExpenseSummary() {
               Purchase Summary
             </h2>
             <hr />
+          </div>
+
+          {/* BODY */}
+          <div className="xl:flex justify-between pr-7">
+            {/* CHART */}
+            <div className="relative basis-3/5 ">
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={expenseCategories}
+                    innerRadius={50}
+                    outerRadius={60}
+                    fill="8884d8"
+                    dataKey={"value"}
+                    nameKey={"name"}
+                    cx={"50%"}
+                    cy={"50%"}
+                  >
+                    {expenseCategories.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={colors[index % colors.length]}
+                      />
+                    ))}
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
           </div>
         </>
       )}
