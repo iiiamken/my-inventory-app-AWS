c0b50fd40ce164033d35142c96e00e29f0cb7499
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-15 22:16:22 +0100

    added body product list

--- client/src/app/products/index.tsx
+++ client/src/app/products/index.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { useGetProductsQuery } from "@/state/api"
-import { SearchIcon } from "lucide-react"
+import { PlusCircleIcon, SearchIcon } from "lucide-react"
 import { useState } from "react"
+import Header from "../(components)/Header"
+import Rating from "../(components)/Rating"
 
 export default function Products() {
   const [searchTerm, setSearchTerm] = useState("")
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setIsModalOpen] = useState(false)
 
   const { data: products, isLoading, isError } = useGetProductsQuery(searchTerm)
 
@@ -33,6 +35,46 @@ export default function Products() {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </div>
+      </div>
+
+      {/* Header Bar */}
+      <Header name="Products" />
+      <button
+        className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create
+        Product
+      </button>
+
+      {/* BODY PRODUCTS LIST */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-10 justify-between">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          products?.map((product) => (
+            <div
+              key={product.productId}
+              className="border shadow rounded-md p-4 max-w-full w-full mx-auto"
+            >
+              <div className="flex flex-col items-center">
+                IMG
+                <h3 className="text-lg text-gray-900 font-semibold">
+                  {product.name}
+                </h3>
+                <p className="text-gray-800">${product.price.toFixed(2)}</p>
+                <div className="text-sm text-gray-600 mt-1">
+                  Stock: {product.stockQuantity}
+                </div>
+                {product.rating && (
+                  <div className="flex items-center mt-2">
+                    <Rating rating={product.rating} />
+                  </div>
+                )}
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   )
