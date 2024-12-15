6d7d3e1e6790fba9245940506c601535215f84c9
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-15 23:33:34 +0100

    created UserSetting type and mockSetting

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -1,3 +1,37 @@
+type UserSettings = {
+  label: string
+  value: string | boolean
+  type: "text" | "toggle"
+}
+
+const mockSettings: UserSettings[] = [
+  {
+    label: "Username",
+    value: "ken D",
+    type: "text",
+  },
+  {
+    label: "Email",
+    value: "kend.dev@example.com",
+    type: "text",
+  },
+  {
+    label: "Notification",
+    value: true,
+    type: "toggle",
+  },
+  {
+    label: "Dark Mode",
+    value: false,
+    type: "toggle",
+  },
+  {
+    label: "Language",
+    value: "English",
+    type: "text",
+  },
+]
+
 export default function Settings() {
   return <div>Settings</div>
 }
