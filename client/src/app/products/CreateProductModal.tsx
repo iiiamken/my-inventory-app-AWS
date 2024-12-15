ee37845cd5361291d2a078dd0fdb1ee3572b0e11
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-15 22:34:50 +0100

    added form and input fields

--- client/src/app/products/CreateProductModal.tsx
+++ client/src/app/products/CreateProductModal.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, FormEvent } from "react"
 import Header from "../(components)/Header"
 import { v4 } from "uuid"
 
@@ -27,11 +28,45 @@ export default function CreateProductModal({
     rating: 0,
   })
 
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    onCreate(formData)
+    onClose()
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]:
+        name === "price" || name === "stockQuantity" || name === "rating"
+          ? parseFloat(value)
+          : value,
+    })
+  }
+
   if (!isOpen) return null
+
+  const labelCssStyles = "block text-sm font-medium text-gray-700"
+  const inputCssStyles =
+    "block w-full mb-2 p-2 border-gray-500 border-2 rounded-md"
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-auto h-full w-full z-20">
       <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
         <Header name={"Create New Product"} />
+        <form onSubmit={handleSubmit}></form>
+        <label htmlFor="productName" className={labelCssStyles}>
+          Product Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+          value={formData.name}
+          className={inputCssStyles}
+          required
+        />
       </div>
     </div>
   )
