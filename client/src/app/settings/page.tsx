40958f469d489ed0dc12f30ff29f9b94cc10bf35
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-15 23:49:06 +0100

    added table

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import { useState } from "react"
+import Header from "../(components)/Header"
 
 type UserSettings = {
   label: string
@@ -41,5 +44,31 @@ export default function Settings() {
     settingsCopy[index].value = !settingsCopy[index].value as boolean
     setUserSettings(settingsCopy)
   }
-  return <div>Settings</div>
+  return (
+    <div className="w-full">
+      <Header name={"User Settings"} />
+      <div className="overflow-x-auto mt-5 shadow-md">
+        <table className="min-w-full bg-white rounded-lg">
+          <thead className="bg-gray-800 text-white">
+            <tr>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+                Setting
+              </th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+                Value
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {userSettings.map((setting, index) => (
+              <tr className="hover:bg-blue-50 " key={setting.label}>
+                <td className="py-2 px-4">{setting.label}</td>
+                <td className="py-2 px-4">{}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
