d44da11c725487a9c45f8c67d781f9d55369815a
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-15 13:53:11 +0100

    created Header component

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -1,8 +1,21 @@
 "use client"
 
 import { useGetProductsQuery } from "@/state/api"
+import Header from "../(components)/Header"
 
 export default function Inventory() {
-  const { data: products, isLoading } = useGetProductsQuery()
-  return <div>Inventory</div>
+  const { data: products, isLoading, isError } = useGetProductsQuery()
+
+  if (isLoading) return <div className="py-4">Loading...</div>
+  if (isError || !products)
+    return (
+      <div className="text-center text-red-500 py-4">
+        Failed to fetch products
+      </div>
+    )
+  return (
+    <div className="flex flex-col">
+      <Header name={"Inventory"} />
+    </div>
+  )
 }
