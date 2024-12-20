41fdf4081dbc849826992facc236da97dfaf7da6
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-20 21:29:00 +0100

    fix

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -5,10 +5,12 @@ import CardExpenseSummary from "./CardExpenseSummary"
 import CardPurchaseSummary from "./CardPurchaseSummary"
 import CardSalesSummary from "./CardSalesSummary"
 import StatCard from "./StatCard"
+import CardPopularProducts from "./cardPopularProducts"
 
 export default function Dashboard() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
       <CardSalesSummary />
       <CardPurchaseSummary />
       <CardExpenseSummary />
